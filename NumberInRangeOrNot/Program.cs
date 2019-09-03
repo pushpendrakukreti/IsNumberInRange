744770b5e1880f4537f0adcb0fbd5f290d685928
PUSHPENDRA KUKRETI <KUKRETIARYAN@GMAIL.COM>
2019-09-04 03:31:02 +0530

    Switch Case Used with Input Theme to Check whether number in Range or Not

--- NumberInRangeOrNot/Program.cs
+++ NumberInRangeOrNot/Program.cs
@@ -10,9 +10,42 @@ namespace NumberInRangeOrNot
     {
         static void Main(string[] args)
         {
-            bool x = InRange(10, 20, 18);
-            Console.WriteLine(x);
-            Console.ReadLine();
+            int ch,i,xx=0,y=0,z=0;
+            for(i=1;i>0;i++)
+            {
+                Console.WriteLine("0. Check Range of Number");
+                Console.WriteLine("1. Exit");
+
+                Console.Write("Enter Your Choice = ");
+                ch=int.Parse(Console.ReadLine());
+                if(ch==1)
+                {
+                    break;
+                }
+                switch(ch)
+                {
+                    case 0:
+                        Console.Write("\nValue of X = ");
+                        xx=int.Parse(Console.ReadLine());
+
+                        Console.Write("Value of Y = ");
+                        y=int.Parse(Console.ReadLine());
+
+                        Console.Write("Value of Z = ");
+                        z=int.Parse(Console.ReadLine());
+
+                        bool x = InRange(xx,y,z);
+                        Console.WriteLine(x);
+                        Console.WriteLine("Enter to Try Again!!\n\n");
+                        Console.ReadKey();
+
+                        break;
+
+                    default:
+                        Console.WriteLine("\nINVALID CHOICE");
+                        break;
+                }
+            }
 
             bool InRange(int high, int low, int num)
             {
