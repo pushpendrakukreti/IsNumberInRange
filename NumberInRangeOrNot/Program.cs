a5263282c16063d5b008b35d65e61d7139f61228
PUSHPENDRA KUKRETI <KUKRETIARYAN@GMAIL.COM>
2019-09-04 02:36:29 +0530

    Checked Simple Comparison like (num>=low&&num<=high) not worked, afterall ((num*high)*(num*low)<=0) that worked

---
+++ NumberInRangeOrNot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInRangeOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = InRange(10, 20, 18);
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+            bool InRange(int high, int low, int num)
+            {
+                //works for negative numbers also.
+                return ((num - high) * (num - low) <= 0);
+            }
+        }
+    }
+}
